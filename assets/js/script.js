c9ba5b258db1e6836851b2ceeb6baed70f159500
Francisco Salazar <149215896+SalazarFco@users.noreply.github.com>
2024-06-13 22:50:26 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,73 @@
-function FiguraGeometrica(lado) {
-  this.lado = lado;
-}
+// Creado por: Francisco Antonio Salazar Candia
+// Fecha: 13-06-2024
 
-function Poligono(lado) {
-  FiguraGeometrica.call(this, lado);
-}
-Poligono.prototype = Object.create(FiguraGeometrica.prototype);
+// (Immediately-invoked Function Expressions
+const ReproductorVideos = (() => {
+  // Función privada
+  const insertarVideo = (url, id) => {
+    const iframe = document.getElementById(id);
+    iframe.setAttribute("src", url);
+  };
 
-Poligono.prototype.calcularArea = function () {
-  return this.lado * this.lado;
-};
+  // Clase Multimedia
+  class Multimedia {
+    constructor(url) {
+      let _url = url; // Implementado closures
 
-var cuadrado = new Poligono(3);
-console.log(cuadrado);
-console.log(cuadrado.calcularArea());
+      this.getURL = () => _url;
+    }
+
+    setInicio() {
+      return "Cambio en la URL del video";
+    }
+  }
+
+  // Clase multimedia Herencia de reproductor
+  class Reproductor extends Multimedia {
+    constructor(url, id) {
+      super(url);
+      this._id = id;
+    }
+
+    playMultimedia() {
+      const url = this.getURL();
+      insertarVideo(url, this._id);
+    }
+
+    // Tiempo de desfase para inincio
+    setInicio(tiempo) {
+      const url = `${this.getURL()}?start=${tiempo}`;
+      insertarVideo(url, this._id);
+    }
+  }
+
+  return {
+    Reproductor,
+  };
+})();
+
+// Pregunta 4. Insatanciar clases
+const { Reproductor } = ReproductorVideos;
+
+const musicaReproductor = new Reproductor(
+  "https://www.youtube.com/embed/YPOJvuV7uKc",
+  "musica"
+);
+const peliculaReproductor = new Reproductor(
+  "https://www.youtube.com/embed/5PSNL1qE6VY",
+  "peliculas"
+);
+const serieReproductor = new Reproductor(
+  "https://www.youtube.com/embed/HhesaQXLuRY",
+  "series"
+);
+
+// Pregunta 5. Invocando el metodo playMultimedia
+musicaReproductor.playMultimedia();
+peliculaReproductor.playMultimedia();
+serieReproductor.playMultimedia();
+
+// Pregunta 6 setInicio para cada uno de los videos. El tiempo será de 15 segundos.
+musicaReproductor.setInicio(15);
+peliculaReproductor.setInicio(15);
+serieReproductor.setInicio(15);
