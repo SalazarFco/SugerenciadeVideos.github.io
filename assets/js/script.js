ad27266121f136afce923a5da3d2d50b02da8fdf
Francisco Salazar <149215896+SalazarFco@users.noreply.github.com>
2024-06-13 22:48:38 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function FiguraGeometrica(lado) {
+  this.lado = lado;
+}
+
+function Poligono(lado) {
+  FiguraGeometrica.call(this, lado);
+}
+Poligono.prototype = Object.create(FiguraGeometrica.prototype);
+
+Poligono.prototype.calcularArea = function () {
+  return this.lado * this.lado;
+};
+
+var cuadrado = new Poligono(3);
+console.log(cuadrado);
+console.log(cuadrado.calcularArea());
